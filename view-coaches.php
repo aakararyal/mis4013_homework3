b7f3fe0a36b6d7ccf8fcfac2503c588e94821225
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 13:48:21 -0500

    Update view-coaches.php

--- view-coaches.php
+++ view-coaches.php
@@ -20,7 +20,7 @@ while ($coach = $coaches->fetch_assoc()) {
     <td><?php echo $coach['coaches_id']; ?></td>
     <td><?php echo $coach['coaches_name']; ?></td>
     <td><?php echo $coach['office_location']; ?></td>
-  <td> <a href = "coaches-and-players.php?id=<?php echo $coach['coaches_id']; ?>">Football Relations</td>
+  <td> <a href = "coaches-with-players.php?id=<?php echo $coach['coaches_id']; ?>">Football Relations</td>
   </tr>
   <?php
 }
