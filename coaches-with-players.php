edc406ab40a16368ebd646c3cc328d8a970d16fe
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 13:39:04 -0500

    Update coaches-with-players.php

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -6,7 +6,7 @@ require_once("model-coaches.php");
 
 $pageTitle = "Coaches with Players";
 include "view-header.php";
-$coaches = selectCoachesWithPlayers($_GET['id]);
+$coaches = selectCoachesWithPlayers($_GET['id']);
 include "view-coaches.php";
 include "view-footer.php";
   ?>
