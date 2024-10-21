1db53d6afec8fad935e05836e5d41e2812d061e4
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 13:16:28 -0500

    Create coaches-with-players.php

---
+++ coaches-with-players.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("util-db.php");
+require_once("model-coaches.php");
+
+
+$pageTitle = "Coaches with Players";
+include "view-header.php";
+$coaches = selectCoachesWithPlayers($_GET['id]);
+include "view-coaches.php";
+include "view-footer.php";
+  ?>
