af2b4c2108c2a8b49d254c48fc32de30da55c8be
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 11:43:34 -0500

    Create view-stadiums.php

---
+++ view-stadiums.php
@@ -0,0 +1,30 @@
+<h1>Stadiums</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+
+      <tr>
+        <th>ID</th>
+      <th>Name</th>
+      <th>Office</th>
+    </thead>
+      </tr>
+  
+
+<tbody>
+  <?php
+while ($stadium = $stadiums->fetch_assoc()) {
+    ?>
+  <tr>
+    <td><?php echo $stadium['stadium_id']; ?></td>
+    <td><?php echo $stadium['stadium_name']; ?></td>
+    <td><?php echo $stadium['staidum_capacity']; ?></td>
+
+  </tr>
+  <?php
+}
+?>
+</tbody>
+    
+  </table>
+</div>
