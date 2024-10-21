d8a3aa21e59a36414361cad5369ff005a2fbfff9
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 11:21:34 -0500

    Update model-coaches.php

--- model-coaches.php
+++ model-coaches.php
@@ -2,7 +2,7 @@
 function selectCoaches() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select coaches_id, coaches_name, office_location from coaches");
+        $stmt = $conn->prepare("SELECT coaches_id, coaches_name, office_location FROM `coaches` ");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
