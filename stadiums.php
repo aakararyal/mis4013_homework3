2a1a0a0de57a9ccb8a0f190eb063050de27eaf28
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-21 11:39:48 -0500

    Create stadiums.php

---
+++ stadiums.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("util-db.php");
+require_once("model-stadiums.php");
+
+
+$pageTitle = "Stadiums";
+include "view-header.php";
+$stadiums = selectStadiums();
+include "view-stadiums.php";
+include "view-footer.php";
+  ?>
