bfb13dacbb2f9ccc63f85e2811068d75ddf81705
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-22 17:53:09 -0500

    Update view-coaches-with-players.php

--- view-coaches-with-players.php
+++ view-coaches-with-players.php
@@ -1,1 +1,32 @@
+<h1>Coaches with Players</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
 
+      <tr>
+        <th>ID</th>
+      <th>Coach Name</th>
+      <th>Office Location</th>
+         <th>NFL Team</th>
+         <th>Division</th>
+    </thead>
+      </tr>
+  
+<tbody>
+  <?php
+while ($coach = $coaches->fetch_assoc()) {
+    ?>
+  <tr>
+    <td><?php echo $coach['coaches_id']; ?></td>
+    <td><?php echo $coach['coaches_name']; ?></td>
+    <td><?php echo $coach['office_location']; ?></td>
+      <td><?php echo $coach['nfl_team']; ?></td>
+    <td><?php echo $coach['division']; ?></td>
+  </tr>
+  <?php
+}
+?>
+</tbody>
+    
+  </table>
+</div>
