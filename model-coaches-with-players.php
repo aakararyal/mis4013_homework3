1cb783aae3824dcc15fd4135e7dad9c59bfce353
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-22 17:29:12 -0500

    Update model-coaches-with-players.php

--- model-coaches-with-players.php
+++ model-coaches-with-players.php
@@ -1,17 +1,1 @@
-<?php
-function selectCoachesWithPlayers($iid) {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT c.coaches_id, coaches_name, office_location, nfl_team, division  FROM `coaches` c join football f on f.coaches_id = c.coaches_id where f.players_id =?");
-       $stmt->bind_param("i", $iid); 
-      $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
 
-?>
