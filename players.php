4fe54744a86879fc033d94ea9c11e5bcb9649d24
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-23 10:24:40 -0500

    Update players.php

--- players.php
+++ players.php
@@ -20,6 +20,16 @@ echo '<div class = "alert alert-success" role = "alert"> Player added.</div>"';
             
       break;
 
+         case "Delete":
+        if (deletePlayer($_POST['cid]) {
+
+echo '<div class = "alert alert-success" role = "alert"> Course deleted.</div>"';
+        } else {
+                echo '<div class = "alert alert-danger" role = "alert"> Error.</div>';
+        }
+            
+      break;
+
     }
   }
 
