0bfcaa15b6aac310b7c0b1f1ac2cdec5c4b343f3
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-23 09:26:35 -0500

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -12,5 +12,46 @@ function selectPlayers() {
         throw $e;
     }
 }
+function insertPlayer($cName, $cPosition) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `players` ( `player_name`, `player_position`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $cName, $cPosition);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateCourse($cName, $cPosition, $cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `players` set `player_name` = ?, `player_position` = ? where players_id = ?");
+        $stmt->bind_param("ssi", $cName, $cPosition, $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlayer($cid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from players where players_id = ?");
+        $stmt->bind_param("i", $cid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 
 ?>
