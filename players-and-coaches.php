238f62de979a59b43f16743c164a3e29a65f7c23
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-22 23:24:18 -0500

    Update players-and-coaches.php

--- players-and-coaches.php
+++ players-and-coaches.php
@@ -6,7 +6,7 @@ require_once("model-players-and-coaches.php");
 
 $pageTitle = "Players and Coaches";
 include "view-header.php";
-$coaches = selectCoachesWithPlayers($_GET['id']);
+$coaches = selectCoaches();
 include "view-players-and-coaches.php";
 include "view-footer.php";
   ?>
