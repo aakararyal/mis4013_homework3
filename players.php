0544f73ce1f7edc7d0d2e3a8b6b616413cbf5631
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-23 10:14:04 -0500

    Update players.php

--- players.php
+++ players.php
@@ -15,7 +15,7 @@ if (isset($_POST['actionType'])) {
 
 echo '<div class = "alert alert-success" role = "alert"> Player added.</div>"';
         } else {
-                echo '<div class = "alert alert-danger" role = "alert"> Error.</div>"';
+                echo '<div class = "alert alert-danger" role = "alert"> Error.</div>';
         }
             
       break;
