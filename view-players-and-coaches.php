34f2b8db0381253f3fe23e8461792b0858cd47ee
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-10-22 23:49:47 -0500

    Update view-players-and-coaches.php

--- view-players-and-coaches.php
+++ view-players-and-coaches.php
@@ -30,12 +30,7 @@ while ($coach = $coaches->fetch_assoc()) {
       <p class="card-text"><small class="text-body-secondary">Location: <?php echo $coach['office_location']; ?></small></p>
     </div>
   
-  <tr>
-    <td><?php echo $coach['coaches_id']; ?></td>
-    <td><?php echo $coach['coaches_name']; ?></td>
-    <td><?php echo $coach['office_location']; ?></td>
-  <td> <a href = "coaches-with-players.php?id=<?php echo $coach['coaches_id']; ?>">NFL players from Coaches</td>
-  </tr>
+ 
   <?php
 }
 ?>
