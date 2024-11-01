acd494dfd7e650ce6ab03ec3131df94e26b4dfa5
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-01 17:28:30 -0500

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -6,6 +6,15 @@ require_once("model-coaches.php");
 
 $pageTitle = "Coaches";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+      case "Add";
+        insertCoaches($_POST['coName'], $_POST['coLocation']);
+        break;
+    }
+  
+}
 $coaches = selectCoaches();
 include "view-coaches.php";
 include "view-footer.php";
