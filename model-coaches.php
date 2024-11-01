5b7407ae01cbbb2a11c5a1cb196bbd422a4759d8
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-01 17:31:58 -0500

    Update model-coaches.php

--- model-coaches.php
+++ model-coaches.php
@@ -16,7 +16,7 @@ function selectCoaches() {
 function insertCoaches($coName, $coLocation) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("INSERT INTO `coaches` (`coaches_name`, `office_location`) VALUES (?, ?) ");
+        $stmt = $conn->prepare("INSERT INTO `coaches` ( `coaches_name`, `office_location`) VALUES (?, ?) ");
         $stmt->bind_param("ss", $coName, $coLocation);
         $success = $stmt->execute();
         $result = $stmt->get_result();
