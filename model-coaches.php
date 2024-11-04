c2e5554c3ff84a3f69f33987b3da2973652c80ea
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-04 00:20:49 -0600

    Update model-coaches.php

--- model-coaches.php
+++ model-coaches.php
@@ -32,7 +32,7 @@ function updateCoaches($coName, $coLocation, $coid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `coaches` set `coaches_name` = ?, `office_location` = ? where coaches_id = ?");
-        $stmt->bind_param(ssi, $coName, $coLocation, $coid);
+        $stmt->bind_param("ssi", $coName, $coLocation, $coid);
         $success = $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
