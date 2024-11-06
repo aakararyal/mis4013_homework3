8250aa4cb544336c6b34f03a5692c726436fc55a
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-05 19:45:11 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -5,12 +5,6 @@
     <script>
 document.body.style.backgroundColor = "red";
 
-
-  <! --  source: https://www.w3schools.com/jsref/jsref_getutcmonth.asp-->  
-
-   const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const d = new Date()
-let month = month[d.getUTCMonth()];
 </script>
     
 
