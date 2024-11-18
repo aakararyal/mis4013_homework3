5facdb07c8e498170f3b86b0afa4de72775183a1
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-18 16:38:28 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -75,7 +75,13 @@ if (isset($_POST['actionType'])) {
              echo '<div class="alert alert-danger" role="alert"> Error. </div>';
         }
         break;
-    }
+    case "Delete";
+        if (deleteCoaches($_POST['coid'])) {
+            echo '<div class="alert alert-success" role="alert"> Coach deleted. </div>';
+        } else {
+             echo '<div class="alert alert-danger" role="alert"> Error. </div>';
+        }
+        break; }
   
 }
 $coaches = selectCoaches();
