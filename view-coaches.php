9e6477d93474ae8962e2aa39f2161ec8ea226c70
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-18 16:36:05 -0600

    Update view-coaches.php

--- view-coaches.php
+++ view-coaches.php
@@ -20,6 +20,8 @@
       <th>Name</th>
       <th>Office</th>
         <th></th>
+         <th></th>
+         <th></th>
     </thead>
       </tr>
   
@@ -32,6 +34,26 @@ while ($coach = $coaches->fetch_assoc()) {
     <td><?php echo $coach['coaches_id']; ?></td>
     <td><?php echo $coach['coaches_name']; ?></td>
     <td><?php echo $coach['office_location']; ?></td>
+
+<td>
+  
+</td>
+
+    <td>
+          <form method = "post" action = "">
+              <input type = "hidden" name = "coid" value = "<?php echo $coach['coaches_id']; ?>">
+            <input type = "hidden" name = "actionType" value = "Delete"
+              <button type = "submit" class = "btn btn-primary">
+
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                                </svg>
+          </button>
+          </form>
+    
+  </td>
+    
   <td> <a href = "coaches-with-players.php?id=<?php echo $coach['coaches_id']; ?>">NFL players from Coaches</td>
   </tr>
   <?php
