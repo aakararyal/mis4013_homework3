52a10ebc749befd1e16b4570b2a573b7d8ed8442
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-19 16:06:50 -0600

    Update view-stadiums-newform.php

--- view-stadiums-newform.php
+++ view-stadiums-newform.php
@@ -27,6 +27,7 @@
               <label for="sCap" class="form-label">Capacity</label>
               <input type="text" class="form-control" id="sCap" name = "sCap">
             </div>
+              <input type = "hidden" name = "actionType" value = "Add">
             <button type="submit" class="btn btn-primary">Save</button>
         </form>
       </div>
