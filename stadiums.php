ebba4f2ff0e7d8dcd7ef744dd9eeb848d2706f27
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-11-19 16:10:03 -0600

    Update stadiums.php

--- stadiums.php
+++ stadiums.php
@@ -6,6 +6,18 @@ require_once("model-stadiums.php");
 
 $pageTitle = "Stadiums";
 include "view-header.php";
+
+
+if (isset($_POST['actionType'])) 
+{
+  switch ($_POST['actionType']) {
+    case "Add":
+    insertStadiums($_POST['sName'], $_POST['sCap']);
+    break;
+  }
+
+}
+
 $stadiums = selectStadiums();
 include "view-stadiums.php";
 include "view-footer.php";
