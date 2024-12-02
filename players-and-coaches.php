419057b6b9ba061917a99623c8c651d3ec025cb2
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-01 23:57:30 -0600

    Update players-and-coaches.php

--- players-and-coaches.php
+++ players-and-coaches.php
@@ -10,7 +10,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
     switch ($_POST['actionType']) {
         case "Add":
-            insertFootball($_POST['coach_id'], $_POST['player_id'], $_POST['stadium_id'], $_POST['nfl_team'], $_POST['seasons'], $_POST['division']);
+            insertFootball($_POST['fName'], $_POST['fCity'], $_POST['fNFL'], $_POST['fDiv']);
             break;
        # case "Edit":
          #   updateFootball($_POST['football_id'], $_POST['coach_id'], $_POST['player_id'], $_POST['stadium_id'], $_POST['nfl_team'], $_POST['seasons'], $_POST['division']);
