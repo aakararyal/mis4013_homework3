21755751b7c7277958ce1e5f7503d98deca0cdbd
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-01 23:54:29 -0600

    Update players-and-coaches.php

--- players-and-coaches.php
+++ players-and-coaches.php
@@ -6,6 +6,23 @@ require_once("model-players-and-coaches.php");
 
 $pageTitle = "Coaches and Former Player Teams";
 include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            insertFootball($_POST['coach_id'], $_POST['player_id'], $_POST['stadium_id'], $_POST['nfl_team'], $_POST['seasons'], $_POST['division']);
+            break;
+       # case "Edit":
+         #   updateFootball($_POST['football_id'], $_POST['coach_id'], $_POST['player_id'], $_POST['stadium_id'], $_POST['nfl_team'], $_POST['seasons'], $_POST['division']);
+           # break;
+       # case "Delete":
+         #  deleteFootball($_POST['football_id']);
+          #  break;
+    }
+}
+
+
+}
 $coaches = selectCoaches();
 include "view-players-and-coaches.php";
 include "view-footer.php";
