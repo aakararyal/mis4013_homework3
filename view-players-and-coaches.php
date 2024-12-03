361217e661a99af5d5caee20f97821504bb3df30
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-03 15:47:22 -0600

    Update view-players-and-coaches.php

--- view-players-and-coaches.php
+++ view-players-and-coaches.php
@@ -34,7 +34,7 @@ while ($coach = $coaches->fetch_assoc()) {
             
             
               <form method = "post" action = "">
-                <input type = "hidden" name = "fid" value = "<?php echo $playe['division']; ?>" >
+                <input type = "hidden" name = "fid" value = "<?php echo $playe['football_id']; ?>" >
                <input type = "hidden" name ="actionType" value = "Delete">
                 <button type = "submit" class = "btn btn-primary"> 
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
