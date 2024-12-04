bc3bec7310407be9d4994128565f0de56119a3ca
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-04 11:28:07 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -27,55 +27,7 @@ document.body.style.backgroundColor = "lightblue";
 </script>
 
 
-<p>Click the button to swap text</p>
 
-<p><button onclick="myFunction()">Click Here!</button></p>
-
-<div id="newDiv">Places</div>
-
-<script>
-function myFunction() {
-  var x = document.getElementById("newDiv");
-  if (x.innerHTML === "Places") {
-    x.innerHTML = "New Information";
-  } else {
-    x.innerHTML = "Places";
-  }
-}
-</script>
-
-<h2>Date Website Made</h2>
-
-<p id="examplenov"></p>
-
-<script>
-const d = new Date("2024-11-05");
-document.getElementById("examplenov").innerHTML = d;
-</script>
-
- <h3>All SEC Conferences</h3>
-
-<p id="demo"></p>
-
-<script>
-const conferences = new Array("SEC ", " Big 10 ", " ACC ", " Big 12 ");
-document.getElementById("demo").innerHTML = conferences;
-</script>
-
-<h3>Teams in Big 10 Using a Map Search</h3>
-
-<p id="newTeams"></p>
-
-<script>
-const midwest = new Map([
-  ["Michigan", 145],
-  ["Ohio State", 134],
-  ["Indiana", 137]
-]);
-
-let teams = midwest.get("Michigan");
-document.getElementById("newTeams").innerHTML = teams + " is the number of years Michigan has been in CFB.";
-</script>
     
 <?php
 
