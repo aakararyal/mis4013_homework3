ca6b017ea80e18a3ce76dc35cd80204f1208402a
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-04 00:33:11 -0600

    Update model-players-and-coaches.php

--- model-players-and-coaches.php
+++ model-players-and-coaches.php
@@ -40,21 +40,6 @@ function selectCoachesWithPlayers($pid) {
     }
 }
 
-function selectCoaches() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT coaches_id, coaches_name, office_location FROM `coaches`");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-
-
 function selectPlayersByCoach($cid) {
     try {
         $conn = get_db_connection();
