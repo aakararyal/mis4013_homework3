c2c46029ae6ef6f15c5bc440dadb494ddf3311d1
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-04 00:49:28 -0600

    Update view-coaches-input-list.php

--- view-coaches-input-list.php
+++ view-coaches-input-list.php
@@ -2,8 +2,13 @@
 
 <?php
 while ($coachItem = $coachList->fetch_assoc()) {
+      $selText = "";
+      if ($selectedCoach == $coachItem['coaches_id']) {
+            $selText = " selected";
+      }
+            
 ?>
-      <option value="<?php echo $coachItem['coaches_id']; ?>">  <?php echo $coachItem['coaches_name']; ?>    </option>
+      <option value="<?php echo $coachItem['coaches_id']; ?>"<?><?=$selText?>>  <?php echo $coachItem['coaches_name']; ?>    </option>
   <?php
 }
 
