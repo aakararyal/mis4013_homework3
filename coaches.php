02b25a59b96c62826f7f430f837d37a013794f2f
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-09 00:17:38 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -19,14 +19,7 @@
    
 
 
-<td>
-    <a href="coaches-with-players.php? id = <?php echo $coach['coaches_id']; ?>"> View Players </a>
-    
-</td>
-    <form method = "GET">
-        <input type = "text" name = "search" placeholder = "Search Coaches " />
-        <button type = "submit">Search</button>
-    </form>
+
 <?php
 
 require_once("util-db.php");
