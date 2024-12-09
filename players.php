bc548b503a1c2c71fd1e34f9198caff12844421a
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-08 23:26:40 -0600

    Update players.php

--- players.php
+++ players.php
@@ -10,14 +10,7 @@
 
         
     </head>
-
-
-    
 <body>
-    <script>
-document.body.style.backgroundColor = "lightblue";
-
-</script>
 <?php
 
 require_once("util-db.php");
@@ -25,7 +18,7 @@ require_once("model-players.php");
 
 
 $pageTitle = "Players";
-include "view/header.php";
+include "view-header.php";
 
 
 if (isset($_POST['actionType'])) {
@@ -64,90 +57,9 @@ echo '<div class = "alert alert-success" role = "alert"> Player edited.</div>"';
   }
 
 $players = selectPlayers();
-
 include "view-players.php";
-
-echo '<div class="container mt-4">
-            <input type="text" id="playerSearch" class="form-control" placeholder="Search for players..." onkeyup="searchPlayers()">
-          </div>';
-
-echo '<div class="container mt-3">
-            <table class="table table-striped table-dark" id="playerTable">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Position</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>';
-
-    foreach ($players as $player) {
-        echo '<tr>
-                <td>' . $player['players_id'] . '</td>
-                <td>' . $player['player_name'] . '</td>
-                <td>' . $player['player_position'] . '</td>
-                <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#playerDetailModal-' . $player['players_id'] . '">
-                        View Player Details
-                    </button>
-                </td>
-              </tr>';
-    }
-
-    echo '  </tbody>
-            </table>
-          </div>';
-   foreach ($players as $player) {
-        echo '<div class="modal fade" id="playerDetailModal-' . $player['players_id'] . '" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Player Details</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Player Name: ' . $player['player_name'] . '</p>
-                            <p>Position: ' . $player['player_position'] . '</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-              </div>';
-    }
-
-
-
-
-
-
-
-
-include "view/footer.php";
+include "view-footer.php";
   ?>
- 
-
-    <script>
-        function searchPlayers() {
-            let input = document.getElementById('playerSearch').value.toLowerCase();
-            let rows = document.getElementById('playerTable').getElementsByTagName('tr');
-
-            for (let i = 1; i < rows.length; i++) {
-                let nameCell = rows[i].getElementsByTagName('td')[1];
-                if (nameCell) {
-                    let name = nameCell.textContent.toLowerCase();
-                    if (name.includes(input)) {
-                        rows[i].style.display = "";
-                    } else {
-                        rows[i].style.display = "none";
-                    }
-                }
-            }
-        }
-    </script>
 
 </body>
 </html>
