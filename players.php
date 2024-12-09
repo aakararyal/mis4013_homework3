24c097382a93007f151c306d907053fa7d111144
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-08 23:21:59 -0600

    Update players.php

--- players.php
+++ players.php
@@ -128,7 +128,7 @@ echo '<div class="container mt-3">
 
 include "view/footer.php";
   ?>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+ 
 
     <script>
         function searchPlayers() {
