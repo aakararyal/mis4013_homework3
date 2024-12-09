0a2c48f9fff9e38c8b8f4b1f587817e5613494ba
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-09 15:19:17 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 
-    <h1><b>Project</b></h1>
     <style>
         body {
             background-image: url('https://cdn11.bigcommerce.com/s-jdhnct1/images/stencil/1280x1280/products/455/1218/football_stadium_horz__53833.1462478985.jpg?c=2');
@@ -12,6 +11,14 @@
         }
         
     </style>
+    <?php
+$pageTitle = "Home";
+include "view/header.php";
+  ?>
+    <h1>Homework 4</h1>
+   <?php
+  include "view/footer.php";
+  ?>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <?php
