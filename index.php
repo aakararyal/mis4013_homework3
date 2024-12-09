647376f7b0f3dc15ae7d378212791864350f57fa
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-08 23:10:44 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
     <head>
 <style>
     body {
-        background-image: url('https://cdn11.bigcommerce.com/s-jdhnct1/images/stencil/1280x1280/products/455/1218/football_stadium_horz__53833.1462478985.jpg?c=2');
-        background-repeat: no repeat;
-        background-position: center;
+        background-image: url('https://cdn11.bigcommerce.com/s-jdhnct1/images/stencil/1280x1280/products/455/1218/football_stadium_horz__53833.1462478985.jpg?c=2');        background-position: center;
     }
 </style>
 
