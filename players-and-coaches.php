78a591876c27ee9a104b5521da59c8c8fa2ed4d0
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-08 23:12:04 -0600

    Update players-and-coaches.php

--- players-and-coaches.php
+++ players-and-coaches.php
@@ -3,13 +3,8 @@
 <head>
 <style>
     body {
-        background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDY1bPjndCB2lBOzONoSuCxg6pYe4AM9fTcw&s');
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background-position: center;
-        font-family: Arial, sans-serif;
-        color: #fff;
+        background-image: url('https://cdn11.bigcommerce.com/s-jdhnct1/images/stencil/1280x1280/products/455/1218/football_stadium_horz__53833.1462478985.jpg?c=2');
+        
     }
 </style>
 
