a2d9ef7902e6badaf0f457e8d752b67efe0ebf41
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-10 01:48:46 -0600

    Update index.php

--- index.php
+++ index.php
@@ -99,5 +99,7 @@ try {
           
         });
     </script>
+
+    <p><h2>The following chart displays the number of players in the table by division and will update as players are added, edited, or deleted.</h2></p>
 </body>
 </html>
