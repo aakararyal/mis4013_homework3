cc25a86a12a815df5f56ed18b8649384bdfa42b2
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-10 00:58:04 -0600

    Update players-by-team.php

--- players-by-team.php
+++ players-by-team.php
@@ -1,3 +1,13 @@
+<html>
+<head>
+<style>
+    body {
+        background-image: url('https://media.istockphoto.com/id/1354705614/photo/gaylord-family-oklahoma-memorial-stadium-at-the-university-of-oklahoma.jpg?s=612x612&w=0&k=20&c=o0zoZKgbg55DJNb5oGQQjmcIcprw7rU3lV7uCgVRTjY=');
+     background-position: center;
+            background-size: cover;
+            text-align: center;
+    }
+</style>
 <?php
 
 require_once("util-db.php");
@@ -10,3 +20,6 @@ $playerteam = selectPlayersByTeam($_POST['cid']);
 include "view-players-by-team.php";
 include "view/footer.php";
   ?>
+
+  
+</html>
