5c8195979d9dd0fdd3a0fc07aa80be31076f9c87
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-10 00:59:28 -0600

    Update coaches-with-players.php

--- coaches-with-players.php
+++ coaches-with-players.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+<style>
+    body {
+        background-image: url('https://media.istockphoto.com/id/1354705614/photo/gaylord-family-oklahoma-memorial-stadium-at-the-university-of-oklahoma.jpg?s=612x612&w=0&k=20&c=o0zoZKgbg55DJNb5oGQQjmcIcprw7rU3lV7uCgVRTjY=');
+     background-position: center;
+            background-size: cover;
+            text-align: center;
+    }
+</style>
+  </head>
 <?php
 
 require_once("util-db.php");
@@ -10,3 +21,4 @@ $coaches = selectCoachesWithPlayers($_GET['id']);
 include "view-coaches-with-players.php";
 include "view/footer.php";
   ?>
+</html>
