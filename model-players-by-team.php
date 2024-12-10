0d478d675a1b5c1aa02e5a3c62f022d57d3728da
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-10 01:46:45 -0600

    Update model-players-by-team.php

--- model-players-by-team.php
+++ model-players-by-team.php
@@ -2,7 +2,7 @@
 function selectPlayersByTeam($cid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT p.players_id, p.player_name, p.seasons, f.nfl_team, f.division  FROM `players` p join football f on p.players_id = f.players_id where f.players_id = ?");
+        $stmt = $conn->prepare("SELECT p.players_id, player_name, seasons, nfl_team, division  FROM `players` p join football f on p.players_id = f.players_id where f.players_id = ?");
        $stmt->bind_param("i", $cid); 
       $stmt->execute();
         $result = $stmt->get_result();
