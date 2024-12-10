3bcb380cc9cbd959af5c0754a338c774af513958
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-09 20:07:17 -0600

    Update coaches.php

--- coaches.php
+++ coaches.php
@@ -1,6 +1,15 @@
 
 <html>
 <head>
+    <style>
+    body {
+        background-image: url('https://media.istockphoto.com/id/1354705614/photo/gaylord-family-oklahoma-memorial-stadium-at-the-university-of-oklahoma.jpg?s=612x612&w=0&k=20&c=o0zoZKgbg55DJNb5oGQQjmcIcprw7rU3lV7uCgVRTjY=');
+        background-position: center;
+            background-size: cover;
+            text-align: center;
+    }
+</style>
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         * {
