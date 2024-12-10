eb859c846a5edce67f930e9702ed83640d6872ba
aakararyal <78166758+aakararyal@users.noreply.github.com>
2024-12-10 00:36:49 -0600

    Update players.php

--- players.php
+++ players.php
@@ -9,7 +9,38 @@
             text-align: center;
     }
 </style>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+         #myInput {
+            background-image: url('/css/searchicon.png');
+            background-position: 10px 12px;
+            background-repeat: no-repeat;
+            width: 100%;
+            font-size: 16px;
+            padding: 12px 20px 12px 40px;
+            border: 1px solid #ddd;
+            margin-bottom: 12px;
+        }
 
+        #myUL {
+            list-style-type: none;
+            padding: 0;
+            margin: 0;
+        }
+
+        #myUL li {
+            border: 1px solid #ddd;
+            margin-top: -1px; /* Prevent double borders */
+            background-color: #f9f9f9;
+            padding: 12px;
+            font-size: 18px;
+            display: block;
+        }
+
+        #myUL li:hover {
+            background-color: #f1f1f1;
+        }
+    </style>
 
         
     </head>
@@ -60,8 +91,40 @@ echo '<div class = "alert alert-success" role = "alert"> Player edited.</div>"';
   }
 
 $players = selectPlayers();
-include "view-players.php";
-include "view/footer.php";
+include "view-players.php"; ?>
+<h2>Filter by Name</h2>
+<input type="text" id="myInput" onkeyup="filterList()" placeholder="Search for players...">
+<ul id="myUL">
+    <?php foreach ($players as $player): ?>
+        <li>
+            <?= $player['player_name'] ?> - <?= $coach['player_position'] ?>
+        </li>
+    <?php endforeach; ?>
+</ul>
+<script>
+    // source https://www.w3schools.com/howto/howto_js_filter_lists.asp
+    function filterList() {
+        var input, filter, ul, li, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        ul = document.getElementById("myUL");
+        li = ul.getElementsByTagName("li");
+
+        for (var i = 0; i < li.length; i++) {
+            txtValue = li[i].textContent || li[i].innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                li[i].style.display = "";
+            } else {
+                li[i].style.display = "none";
+            }
+        }
+    }
+</script>
+
+
+
+
+<?php include "view/footer.php";
   ?>
 
 </body>
